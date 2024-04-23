808540f83496531c77a3b27cb03521b06ce6a42f
Till <Till.Seifert@d-fine.de>
2024-04-23 13:41:22 +0100

    Finalising Task 1, started with task 2, but disabled

--- src/main/java/com/oocode/CalorieCounting.java
+++ src/main/java/com/oocode/CalorieCounting.java
@@ -1,10 +1,39 @@
 package com.oocode;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
-public class CalorieCounting { public int answer(String  input) {
-        //return Integer.parseInt(input);
-        String[] arrCalories = input.split("\n");
-        return  Arrays.stream(arrCalories).map(Integer::parseInt).reduce(0, Integer::sum);
+public class CalorieCounting {
+    public int answer(String  input) throws IllegalArgumentException {
+        String[] arrElves = input.split("\n\n");
+        return Arrays.stream(arrElves).map(s -> s.split("\n"))
+                .map(arrCalories -> Arrays.stream(arrCalories)
+                        .map(Integer::parseInt)
+                        .peek(item -> {
+                            if (item < 0) {
+                                throw  new IllegalArgumentException("Cannot be negative");
+                            }
+                        })
+                        .reduce(0, Integer::sum))
+                .reduce(0, Integer::max);
+    }
+
+    public int answer2(String  input) throws IllegalArgumentException {
+        String[] arrElves = input.split("\n\n");
+        return Arrays.stream(arrElves).map(s -> s.split("\n"))
+                .map(arrCalories -> Arrays.stream(arrCalories)
+                        .map(Integer::parseInt)
+                        .peek(item -> {
+                            if (item < 0) {
+                                throw  new IllegalArgumentException("Cannot be negative");
+                            }
+                        })
+                        .reduce(0, Integer::sum))
+                .sorted()
+                .collect(Collectors.toList())
+                .reversed()
+                .subList(0,3)
+                .stream()
+                .reduce(0, Integer::sum);
     }
 }
